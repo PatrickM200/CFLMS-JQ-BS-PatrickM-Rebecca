1b324237a588e8ed075db14d56eab1d2c6131c8a
Patrick Miglar <miglar.patrick@gmail.com>
2020-06-15 15:08:26 +0200

    update

--- Challenge/js/mainscript.js
+++ Challenge/js/mainscript.js
@@ -4,12 +4,11 @@ $(document).ready(function(){
     $("#wrapper").html("");
     for (let i=0; i < video.length; i++) {
             $("#wrapper").append(`
-         <figure class="video col-lg-2 col-md-4 col-sm-6 ">
-            <iframe width="560" height="315" src="${video.video}" frameborder="0" 
+    <figure class="video col-lg-2 col-md-4 col-sm-6">
+            <iframe class="if" src="${video.video}" frameborder="0" 
             allow="accelerometer; autoplay; encrypted-media; gyroscope; 
             picture-in-picture" allowfullscreen></iframe>
             <figcaption>${video.videoTitle}</figcaption>
-        </figure> 
-    `);
-    };   
-}) 
+    </figure> `);
+    };
+    });       
