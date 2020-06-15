b6275c7867e1db1324330305f9c0a762d9dd86c5
Patrick Miglar <miglar.patrick@gmail.com>
2020-06-15 15:58:33 +0200

    fixed videos and remove test img

--- Challenge/js/mainscript.js
+++ Challenge/js/mainscript.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
     for (let i=0; i < video.length; i++) {
             $("#wrapper").append(`
     <figure class="video col-lg-2 col-md-4 col-sm-6">
-            <iframe class="if" src="${video.video}" frameborder="0" 
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; 
+            <iframe src="${video[i].video}" frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope;
             picture-in-picture" allowfullscreen></iframe>
-            <figcaption>${video.videoTitle}</figcaption>
+            <figcaption>${video[i].videoTitle}</figcaption>
     </figure> `);
     };
-    });       
+    });  
