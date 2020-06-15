ee883865d44a9858d0f5d4abada8e529480bb8cb
Rebecca Schedler <rebecca.schedler@gmx.net>
2020-06-15 14:21:34 +0200

    work in progress

--- Challenge/js/mainscript.js
+++ Challenge/js/mainscript.js
@@ -2,15 +2,14 @@
 $(document).ready(function(){
     let video = JSON.parse(data);
     $("#wrapper").html("");
-    for (let i=0; i < movie.length; i++) {
+    for (let i=0; i < video.length; i++) {
             $("#wrapper").append(`
-    <div class="video">
-        <figure>
+         <figure class="video col-lg-2 col-md-4 col-sm-6 ">
             <iframe width="560" height="315" src="${video.video}" frameborder="0" 
             allow="accelerometer; autoplay; encrypted-media; gyroscope; 
             picture-in-picture" allowfullscreen></iframe>
             <figcaption>${video.videoTitle}</figcaption>
         </figure> 
-    </div>`);
-    };
-    };       
+    `);
+    };   
+}) 
